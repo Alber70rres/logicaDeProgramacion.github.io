24eefaab8a62e0050db79f50f82334cc2d3b288d
Ortorres321 <118079557+Ortorres321@users.noreply.github.com>
2023-04-25 11:59:30 -0500

    Add files via upload
    first commint

---
+++ main.js
@@ -0,0 +1,24 @@
+"use strict";
+const aparecer = document.querySelector(".desplegar");
+const abrir = document.querySelector(".icono");
+abrir.addEventListener("click",()=>{
+    aparecer.classList.toggle("aparecer");
+    document.body.style.overflowY= "hidden";
+})
+
+const cerrar = document.querySelector(".cerrar");
+cerrar.addEventListener("click",()=>{
+    aparecer.classList.toggle("aparecer");
+    document.body.style.overflowY= "auto";
+})
+
+const myComponent = document.querySelector('.barra');
+const desplegar = document.querySelector('.desplegar');
+
+desplegar.addEventListener('click', (event) => {
+  const isClickedInside = myComponent.contains(event.target);
+  if (isClickedInside == false) {
+    aparecer.classList.toggle("aparecer");
+    document.body.style.overflowY= "auto";
+  }
+});
